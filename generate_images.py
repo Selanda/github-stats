4c4853ba0422c28db555f19275d27b022f5b248e
Selanda <64166375+Selanda@users.noreply.github.com>
2020-11-27 13:49:30 -0500

    Manual adjustment from master

--- generate_images.py
+++ generate_images.py
@@ -75,7 +75,6 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
 <span class="lang">{lang}</span>
 <span class="percent">{data.get("prop", 0):0.2f}%</span>
 </li>
-
 """
 
     output = re.sub(r"{{ progress }}", progress, output)
